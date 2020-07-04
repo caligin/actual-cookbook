39d604cddecc2a3a96a7c4f899943cb18c377968
0xf00 <foo@anima.tech>
2020-07-04 11:53:33 +0100

    enh: sketched renderer tool

--- cookbookc/src/main.rs
+++ cookbookc/src/main.rs
@@ -1,23 +1,48 @@
 extern crate handlebars;
-// #[macro_use]
 extern crate serde_yaml;
-//extern crate serde_json;
 
 use handlebars::Handlebars;
 use serde_yaml::Value;
 use std::error::Error;
+use std::ffi::OsStr;
+use std::fs::read_dir;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut reg = Handlebars::new();
-    // render without register
-    let data: Value = serde_yaml::from_str("name: foo")?;
-    println!(
-        "{}",
-        reg.render_template("Hello {{name}}", &data)?
-    );
+    let recipes = read_dir(Path::new("../src"))
+        .unwrap()
+        .map(|r| r.unwrap())
+        .filter(|e| e.path().extension() == Some(OsStr::new("yml")))
+        .map(|e| serde_yaml::from_reader(BufReader::new(File::open(e.path()).unwrap())).unwrap())
+        .collect::<Vec<Value>>();
 
-    // register template using given name
-    reg.register_template_string("tpl_1", "Good afternoon, {{name}}")?;
-    println!("{}", reg.render("tpl_1", &data)?);
+    // TODO validate that they have all required fields and structure
+
+    // TODO find a clever way to use tags
+    reg.register_template_string("recipe",
+r#"# {{recipe.title}}
+
+{{recipe.description}}
+## preparation
+
+{{recipe.preparation}}
+## ingredients
+
+{{#each recipe.ingredients}}
+- {{this.amount}}{{this.unit}} {{this.name}}
+{{/each}}
+## notes
+
+{{recipe.notes}}"#)?;
+    for recipe in recipes.iter() {
+        let filename = recipe["recipe"]["title"].as_str().unwrap().to_lowercase().replace(" ", "-") + ".md";
+        let recipe_path = Path::new("../").join(recipe["recipe"]["section"].as_str().unwrap()).join(filename);
+        let recipe_md = format!("{}", reg.render("recipe", recipe)?);
+        File::create(recipe_path).unwrap().write_all(recipe_md.as_bytes())?;
+    }
     Ok(())
 }
---
+++ drinks/test-drink.md
@@ -0,0 +1,25 @@
+# test drink
+
+some blurb about the things
+
+## preparation
+
+some text
+and then some more text
+
+linebreak right? ^
+maybe a markdown [link](https://protocol.kitchen) too and *some* _emphasis_ `even`
+
+## ingredients
+
+
+- 60ml bourbounnn
+
+- 60ml simple syrup
+
+## notes
+
+some
+ramblings
+
+asdasd
